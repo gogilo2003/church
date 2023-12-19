1d665aabc0a4f4beed711946c4a2f3a240f0f8d1
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2023-12-19 19:01:59 +0300

    Fixed a bug that prevented Swal from firing without displaying any error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -10,7 +10,6 @@ module.exports = {
         './resources/js/**/*.vue',
         "./node_modules/flowbite/**/*.js",
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -18,6 +17,6 @@ module.exports = {
             },
         },
     },
-
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('flowbite/plugin')],
-};
+}
+
