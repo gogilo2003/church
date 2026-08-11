7fe8d56d0a16c272a6258805264ce1f01a555857
Ogilo George <gogilo2003@hotmail.com>
2026-08-11 09:27:46 +0300

    fix(testing): normalize parallel testing sqlite database filename format

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (class_exists(\Illuminate\Support\Facades\ParallelTesting::class) && \Illuminate\Support\Facades\ParallelTesting::token()) {
+            $db = config('database.connections.sqlite.database');
+            if (is_string($db) && preg_match('/^(.*)\.sqlite_test_(\d+)$/', $db, $matches)) {
+                $fixedDb = $matches[1] . '_test_' . $matches[2] . '.sqlite';
+                config(['database.connections.sqlite.database' => $fixedDb]);
+            }
+        }
+    }
 }
